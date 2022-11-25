577a6a972dc392829237f000e8caf8d278623830
Eric Rosetti Lessa <ericrlessa@gmail.com>
2022-11-25 17:02:51 -0300

    sonar code smells

--- src/index.js
+++ src/index.js
@@ -9,25 +9,25 @@ app.get('/', (req,res) => {
 })
 
 app.get('/sum', (req,res) => {
-    a = Number(req.query.a) 
-    b = Number(req.query.b) 
+    var a = Number(req.query.a) 
+    var b = Number(req.query.b) 
     res.json({message: 'Sum Calculator Rocks!', result: calc.sum(a, b)})
 })
 
 app.get('/sub', (req,res) => {
-    a = Number(req.query.a) 
-    b = Number(req.query.b) 
+    var a = Number(req.query.a) 
+    var b = Number(req.query.b) 
     res.json({message: 'Sub Calculator Rocks!', result: calc.sub(a, b)})
 })
 
 app.get('/times', (req,res) => {
-    a = Number(req.query.a) 
-    b = Number(req.query.b) 
+    var a = Number(req.query.a) 
+    var b = Number(req.query.b) 
     res.json({message: 'Times Calculator Rocks!', result: calc.times(a, b)})
 })
 
 app.get('/squared', (req,res) => {
-    a = Number(req.query.a) 
+    var a = Number(req.query.a) 
     res.json({message: 'Squared Calculator Rocks!', result: calc.squared(a)})
 })
 
