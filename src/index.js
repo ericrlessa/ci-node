b690841343fe36943902d917cc41482adfd406a2
Eric Rosetti Lessa <ericrlessa@gmail.com>
2022-11-25 16:46:57 -0300

    integration test

--- src/index.js
+++ src/index.js
@@ -11,26 +11,29 @@ app.get('/', (req,res) => {
 app.get('/sum', (req,res) => {
     a = Number(req.query.a) 
     b = Number(req.query.b) 
-    res.send("" + calc.sum(a, b))
+    res.json({message: 'Sum Calculator Rocks!', result: calc.sum(a, b)})
 })
 
 app.get('/sub', (req,res) => {
     a = Number(req.query.a) 
     b = Number(req.query.b) 
-    res.send("" + calc.sub(a, b))
+    res.json({message: 'Sub Calculator Rocks!', result: calc.sub(a, b)})
 })
 
 app.get('/times', (req,res) => {
     a = Number(req.query.a) 
     b = Number(req.query.b) 
-    res.send("" + calc.times(a, b))
+    res.json({message: 'Times Calculator Rocks!', result: calc.times(a, b)})
 })
 
 app.get('/squared', (req,res) => {
     a = Number(req.query.a) 
-    res.send("" + calc.squared(a))
+    res.json({message: 'Squared Calculator Rocks!', result: calc.squared(a)})
 })
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
+var server = app.listen(port, ()=> {
 })
+
+
+module.exports = server
+
